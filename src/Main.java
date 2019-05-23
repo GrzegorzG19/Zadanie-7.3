7e021cc51138a267ff0c26b70b2163429b5ef66b
Grzegorz <grzegorzgolan18@gmail.com>
2019-05-23 17:18:05 +0200

    2

--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,14 @@
 public class Main {
     public static void main(String[] args) {
         int suma = 0;
-        for(int i = 2; i <=100;i++ ){
+        for (int i = 2; i <= 100; i++) {
 
-            if (i%2==0){
-                suma+= i;
-                System.out.printf("%d ",i);
+            if (i % 2 == 0) {
+                suma += i;
+                System.out.printf("%d ", i);
             }
 
         }
-        System.out.printf("\nSuma %d ",suma);
+        System.out.printf("\nSuma %d ", suma);
     }
 }
